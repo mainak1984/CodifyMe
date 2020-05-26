aa0100a12d5f732f7f94f9afb21a023a85b93541
Mainak Choudhury <its.mainak2003@gmail.com>
2020-05-26 19:20:47 +0530

    Interview: Google deck day 4_2

---
+++ src/edu/codifyme/leetcode/interview/googledeck/sortnsearch/MedianOfTwoSortedArray.java
@@ -0,0 +1,58 @@
+package edu.codifyme.leetcode.interview.googledeck.sortnsearch;
+
+/**
+ * HARD:
+ * https://leetcode.com/explore/interview/card/google/63/sorting-and-searching-4/3080/
+ *
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ * You may assume nums1 and nums2 cannot be both empty.
+ *
+ * Example 1:
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * The median is 2.0
+ *
+ * Example 2:
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * The median is (2 + 3)/2 = 2.5
+ *
+ * The key point of this problem is to ignore half part of A and B each step recursively by comparing the median of remaining A and B:
+ * if (aMid < bMid) Keep [aRight + bLeft]
+ * else Keep [bRight + aLeft]
+ */
+public class MedianOfTwoSortedArray {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int len = m + n;
+        if(len % 2 == 0){
+            double left =  (double)findKthHelper(nums1, 0, nums2, 0, len/2);
+            double right =  (double)findKthHelper(nums1, 0, nums2, 0, len/2 + 1);
+            return (double)(left + right)/2;
+        }else{
+            return findKthHelper(nums1, 0, nums2, 0, len/2 + 1);
+        }
+    }
+    private int findKthHelper(int[] A, int aStart, int[] B, int bStart, int k){
+        if(aStart >= A.length){
+            return B[bStart + k - 1];
+        }
+        if(bStart >= B.length){
+            return A[aStart + k - 1];
+        }
+        if(k == 1){
+            return Math.min(A[aStart], B[bStart]);
+        }
+        int aMid = aStart + k/2 - 1;
+        int bMid = bStart + k/2 - 1;
+        int aVal = aMid >= A.length ? Integer.MAX_VALUE : A[aMid];
+        int bVal = bMid >= B.length ? Integer.MAX_VALUE : B[bMid];
+        if(aVal <= bVal){
+            return findKthHelper(A, aMid + 1, B, bStart, k - k/2);
+        }else{
+            return findKthHelper(A, aStart, B, bMid + 1, k - k/2);
+        }
+    }
+}
