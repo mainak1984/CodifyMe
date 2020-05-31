73987f2513a4601c3444212c86d92b462c398414
Mainak Choudhury <its.mainak2003@gmail.com>
2020-06-01 01:37:48 +0530

    Interview: Google deck day 6_5

--- src/edu/codifyme/leetcode/interview/googledeck/treesngrapsh/MostStonesRemovedWithSameRowOrColumn.java
+++ src/edu/codifyme/leetcode/interview/googledeck/treesngrapsh/MostStonesRemovedWithSameRowOrColumn.java
@@ -24,6 +24,15 @@ import java.util.Set;
  * Example 3:
  * Input: stones = [[0,0]]
  * Output: 0
+ *
+ * Approach: 1
+ * Count number of island. The main insight is that we can always make moves that reduce the number of stones in each
+ * component to 1. So, total connection possible = stoneNum - islandNum
+ *
+ * Approach: 2
+ * As in Approach 1, we will need to consider components of an underlying graph. A "Disjoint Set Union" (DSU) data
+ * structure is ideal for this.
+ *
  */
 public class MostStonesRemovedWithSameRowOrColumn {
     public int removeStones(int[][] stones) {
@@ -66,4 +75,36 @@ public class MostStonesRemovedWithSameRowOrColumn {
             }
         }
     }
+
+    // APPROACH: 2: Union Find
+//    public int removeStones(int[][] stones) {
+//        int N = stones.length;
+//        DSU dsu = new DSU(20000);
+//
+//        for (int[] stone: stones)
+//            dsu.union(stone[0], stone[1] + 10000);
+//
+//        Set<Integer> seen = new HashSet();
+//        for (int[] stone: stones)
+//            seen.add(dsu.find(stone[0]));
+//
+//        return N - seen.size();
+//    }
+//}
+//
+//class DSU {
+//    int[] parent;
+//    public DSU(int N) {
+//        parent = new int[N];
+//        for (int i = 0; i < N; ++i)
+//            parent[i] = i;
+//    }
+//    public int find(int x) {
+//        if (parent[x] != x) parent[x] = find(parent[x]);
+//        return parent[x];
+//    }
+//    public void union(int x, int y) {
+//        parent[find(x)] = find(y);
+//    }
+//}
 }
