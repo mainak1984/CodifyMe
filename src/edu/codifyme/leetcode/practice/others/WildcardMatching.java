adb0c0d3062cce2ca7ad1aee0c890010ce3db4f5
Mainak Choudhury <its.mainak2003@gmail.com>
2020-07-08 14:55:40 +0530

    Interview: Leetcode practice review 2

--- src/edu/codifyme/leetcode/practice/others/WildcardMatching.java
+++ src/edu/codifyme/leetcode/practice/others/WildcardMatching.java
@@ -3,12 +3,57 @@ package edu.codifyme.leetcode.practice.others;
 /**
  * HARD: Wildcard Matching
  * https://leetcode.com/problems/wildcard-matching
- * https://www.programcreek.com/2014/06/leetcode-wildcard-matching-java/
  *
- * Implement wildcard pattern matching with support for '?' and '*'.
+ * Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
  *
- * Java Solution
- * To understand this solution, you can use s="aab" and p="*ab".
+ * '?' Matches any single character.
+ * '*' Matches any sequence of characters (including the empty sequence).
+ * The matching should cover the entire input string (not partial).
+ *
+ * Note:
+ *
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+ * Example 1:
+ *
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * Example 2:
+ *
+ * Input:
+ * s = "aa"
+ * p = "*"
+ * Output: true
+ * Explanation: '*' matches any sequence.
+ * Example 3:
+ *
+ * Input:
+ * s = "cb"
+ * p = "?a"
+ * Output: false
+ * Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+ * Example 4:
+ *
+ * Input:
+ * s = "adceb"
+ * p = "*a*b"
+ * Output: true
+ * Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
+ * Example 5:
+ *
+ * Input:
+ * s = "acdcb"
+ * p = "a*c?b"
+ * Output: false
+ *
+ * Solution:
+ * For ? and matching character, move pointer for both string and pattern
+ * For *, mark both sPos and pPos and move pattern
+ * For any non matching char, if there exists a valid sPos, reset pattern index to sPos + 1, move pPos to saved pPos and increase pPos
+ * For non matching char, if there is no previous *, return false
  */
 public class WildcardMatching {
     public boolean isMatch(String s, String p) {
