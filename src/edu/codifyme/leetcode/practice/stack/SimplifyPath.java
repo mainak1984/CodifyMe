936170f106893d6097089ca99bc9527a7fd3716b
Mainak Choudhury <its.mainak2003@gmail.com>
2020-07-08 23:44:40 +0530

    Interview: Leetcode practice review 10

---
+++ src/edu/codifyme/leetcode/practice/stack/SimplifyPath.java
@@ -0,0 +1,99 @@
+package edu.codifyme.leetcode.practice.stack;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * MEDIUM: Simplify Path
+ * https://leetcode.com/problems/simplify-path/
+ *
+ * Given an absolute path for a file (Unix-style), simplify it. Or in other words, convert it to the canonical path.
+ * In a UNIX-style file system, a period . refers to the current directory. Furthermore, a double period .. moves the
+ * directory up a level.
+ * Note that the returned canonical path must always begin with a slash /, and there must be only a single slash /
+ * between two directory names. The last directory name (if it exists) must not end with a trailing /. Also, the
+ * canonical path must be the shortest string representing the absolute path.
+ *
+ * Example 1:
+ * Input: "/home/"
+ * Output: "/home"
+ * Explanation: Note that there is no trailing slash after the last directory name.
+ *
+ * Example 2:
+ * Input: "/../"
+ * Output: "/"
+ * Explanation: Going one level up from the root directory is a no-op, as the root level is the highest level you can go.
+ *
+ * Example 3:
+ * Input: "/home//foo/"
+ * Output: "/home/foo"
+ * Explanation: In the canonical path, multiple consecutive slashes are replaced by a single one.
+ *
+ * Example 4:
+ * Input: "/a/./b/../../c/"
+ * Output: "/c"
+ *
+ * Example 5:
+ * Input: "/a/../../b/../c//.//"
+ * Output: "/c"
+ *
+ * Example 6:
+ * Input: "/a//b////c/d//././/.."
+ * Output: "/a/b/c"
+ *
+ * Solution:
+ * Use a stack to push literals when they are word, pop when they are ‘..’, do nothing when they are ‘’ or ‘.’
+ * Once the stack is traversed, use the leftover elements to form the path.
+ */
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<String>();
+
+        //stack.push(path.substring(0,1));
+
+        while(path.length()> 0 && path.charAt(path.length()-1) =='/'){
+            path = path.substring(0, path.length()-1);
+        }
+
+        int start = 0;
+        for(int i=1; i<path.length(); i++){
+            if(path.charAt(i) == '/'){
+                stack.push(path.substring(start, i));
+                start = i;
+            }else if(i==path.length()-1){
+                stack.push(path.substring(start));
+            }
+        }
+
+        LinkedList<String> result = new LinkedList<String>();
+        int back = 0;
+        while(!stack.isEmpty()){
+            String top = stack.pop();
+
+            if(top.equals("/.") || top.equals("/")){
+                //nothing
+            }else if(top.equals("/..")){
+                back++;
+            }else{
+                if(back > 0){
+                    back--;
+                }else{
+                    result.push(top);
+                }
+            }
+        }
+
+        //if empty, return "/"
+        if(result.isEmpty()){
+            return "/";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!result.isEmpty()){
+            String s = result.pop();
+            sb.append(s);
+        }
+
+        return sb.toString();
+    }
+}
