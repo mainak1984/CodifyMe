e8a1c140134e702cabf1e7da4266d690fdd5754c
Mainak Choudhury <its.mainak2003@gmail.com>
2020-07-08 20:04:04 +0530

    Interview: Leetcode practice review 8

---
+++ src/edu/codifyme/leetcode/practice/array/ContainsDuplicateII.java
@@ -0,0 +1,42 @@
+package edu.codifyme.leetcode.practice.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * EASY: Contains Duplicate II
+ * https://leetcode.com/problems/contains-duplicate-ii/
+ *
+ * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array
+ * such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+ * Example 2:
+ *
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ * Example 3:
+ *
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
+ *
+ * Solution:
+ * Use hashmap or hash set to check if the same number is present at k distance.
+ */
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(i > k)
+                set.remove(nums[i-k-1]);
+
+            if(!set.add(nums[i]))
+                return true;
+        }
+        return false;
+    }
+}
