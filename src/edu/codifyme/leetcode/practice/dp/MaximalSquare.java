78aa9fefdc5491e01f03688df75e10d48d5ac3b7
Mainak Choudhury <its.mainak2003@gmail.com>
2020-07-16 23:53:14 +0530

    Interview: Mock interview 8

--- src/edu/codifyme/leetcode/practice/dp/MaximalSquare.java
+++ src/edu/codifyme/leetcode/practice/dp/MaximalSquare.java
@@ -50,4 +50,46 @@ public class MaximalSquare {
 
         return maxVal * maxVal;
     }
+
+    // Efficient implementation
+//    public int maximalSquare(char[][] matrix) {
+//        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+//        int[][] dp = new int[rows + 1][cols + 1];
+//        int maxsqlen = 0;
+//        for (int i = 1; i <= rows; i++) {
+//            for (int j = 1; j <= cols; j++) {
+//                if (matrix[i-1][j-1] == '1'){
+//                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+//                    maxsqlen = Math.max(maxsqlen, dp[i][j]);
+//                }
+//            }
+//        }
+//        return maxsqlen * maxsqlen;
+//    }
+
+    // More eeficient DP
+//    In the previous approach for calculating dp of ith row we are using only the previous element and the (i-1)th row.
+//    Therefore, we don't need 2D dp matrix as 1D dp array will be sufficient for this.
+//    Initially the dp array contains all 0's. As we scan the elements of the original matrix across a row, we keep on
+//    updating the dp array as per the equation dp[j]=min(dp[j-1],dp[j],prev), where prev refers to the old dp[j-1]].
+//    For every row, we repeat the same process and update in the same dp array.
+//    
+//    public int maximalSquare(char[][] matrix) {
+//        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+//        int[] dp = new int[cols + 1];
+//        int maxsqlen = 0, prev = 0;
+//        for (int i = 1; i <= rows; i++) {
+//            for (int j = 1; j <= cols; j++) {
+//                int temp = dp[j];
+//                if (matrix[i - 1][j - 1] == '1') {
+//                    dp[j] = Math.min(Math.min(dp[j - 1], prev), dp[j]) + 1;
+//                    maxsqlen = Math.max(maxsqlen, dp[j]);
+//                } else {
+//                    dp[j] = 0;
+//                }
+//                prev = temp;
+//            }
+//        }
+//        return maxsqlen * maxsqlen;
+//    }
 }
